d0f15de41cffcebf790910f07c61f5b3e35d21fa
Frank <frankz@eyebuydirect.com>
2019-11-29 14:57:55 +0800

    test time zone

--- controller/AbstractController.php
+++ controller/AbstractController.php
@@ -20,6 +20,7 @@
             switch ($name) {
                 case 'db':
                     $this->temp['db'] = new NewPdo('mysql:dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' . DB_PORT, DB_USERNAME, DB_PASSWORD);
+                    $this->temp['db']->exec("SET time_zone = 'Asia/Shanghai'");
                     break;
                 case 'model':
                     $this->temp['model'] = new Model();
