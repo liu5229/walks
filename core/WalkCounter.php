9affcf798ec548904748e00edf00c9b8e853c3d0
faaat <zhengjianfeng@Frankz.local>
2019-11-30 20:21:10 +0800

    walk

--- core/WalkCounter.php
+++ core/WalkCounter.php
@@ -35,7 +35,9 @@ class walkCounter extends AbstractModel
         
         $sql = 'SELECT receive_id id, receive_gold num, receive_type type, receive_status isReceived FROM t_gold2receive WHERE user_id = ? AND receive_date = ? AND receive_type = "walk_stage"';
         $walkStage = $this->db->getAll($sql, $this->userId, $this->todayDate);
-        return array('awardCoins1' => $walk, 'awardCoins2' => $walkStage, 'stageReward' => array_map(function($v, $key){$key = 'step_' . $key;}, $this->stageReward));
+        $stageReward = array();
+        array_walk($this->stageReward, function($v, $key) use (&$stageReward) {$stageReward['step' . $key] =$v;});
+        return array('awardCoins1' => $walk, 'awardCoins2' => $walkStage, 'stageReward' => $stageReward);
     }
     
     protected function calculationReward() {
