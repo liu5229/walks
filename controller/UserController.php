943a55f63f22903d74b75417c6d1fe655e3a5645
faaat <zhengjianfeng@Frankz.local>
2019-12-15 14:41:21 +0800

    add version

--- controller/UserController.php
+++ controller/UserController.php
@@ -62,7 +62,7 @@ Class UserController extends AbstractController {
         if (!isset($this->inputData['location']) && !in_array($this->inputData['location'], array_keys($adCount))) {
             return new ApiReturn('', 305, '没有广告位置');
         }
-        $sql = 'SELECT advertise_type type, CONCAT(?, ?, advertise_image) img, advertise_url url FROM t_advertise WHERE advertise_location = ? AND advertise_status = 1 ORDER BY advertise_id DESC LIMIT ?';
-        return new ApiReturn($this->db->getAll($sql, HOST_NAME, IMG_DIR, $this->inputData['location'], $adCount[$this->inputData['location']]));
+        $sql = 'SELECT advertise_type type, CONCAT(?, ?, advertise_image) img, advertise_url url FROM t_advertise WHERE advertise_location = ? AND advertise_status = 1 ORDER BY advertise_id DESC LIMIT ' . $adCount[$this->inputData['location']];
+        return new ApiReturn($this->db->getAll($sql, HOST_NAME, IMG_DIR, $this->inputData['location']));
     }
 }
