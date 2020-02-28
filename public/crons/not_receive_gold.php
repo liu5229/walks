fe1195e79749a2d55a484ef55d8f354516953c89
faaat <zjf580@163.com>
2020-02-28 16:40:07 +0800

    fix crons

--- public/crons/not_receive_gold.php
+++ public/crons/not_receive_gold.php
@@ -1,7 +1,7 @@
 <?php
 
-//删除两周前未领取的用户接收到金币  这些数据也没有任何作用
-//每周一1：00执行一次
+//删除昨天前未领取的用户接收到金币  这些数据也没有任何作用
+//每天1：00执行一次
 
 require_once __DIR__ . '/../init.inc.php';
 
@@ -9,7 +9,8 @@ $db = new NewPdo('mysql:dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' .
 $db->exec("SET time_zone = 'Asia/Shanghai'");
 $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
-$lastWeekDate = date('Y-m-d 00:00:00', strtotime('-2 week'));
-$sql = 'DELETE FROM t_gold2receive WHERE create_time < ? AND receive_status = 0';
+$lastWeekDate = date('Y-m-d', strtotime('-1 day'));
+$sql = 'DELETE FROM t_gold2receive WHERE receive_date <= ? AND receive_status = 0';
 $db->exec($sql, $lastWeekDate);
+
 echo 'done';
