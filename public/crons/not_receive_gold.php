62fcc876de464eebc8e72cb1ca2647fa8f9cc48b
faaat <zjf580@163.com>
2020-02-29 13:08:46 +0800

    fix receive table

--- public/crons/not_receive_gold.php
+++ public/crons/not_receive_gold.php
@@ -9,16 +9,16 @@ $db = new NewPdo('mysql:dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' .
 $db->exec("SET time_zone = 'Asia/Shanghai'");
 $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
-$lastWeekDate = date('Y-m-d', strtotime('-1 day'));
-echo $lastWeekDate;
+$lastDayTime = date('Y-m-d 23:59:59', strtotime('-1 day'));
+
 $sql = 'INSERT INTO t_gold2receive_old(receive_id, user_id, receive_gold, receive_walk, receive_type, receive_status, end_time, is_double, receive_date, create_time)
         SELECT receive_id, user_id, receive_gold, receive_walk, receive_type, receive_status, end_time, is_double, receive_date, create_time
-        FROM t_gold2receive WHERE receive_date <= ? AND receive_status = 1';
-$return = $db->exec($sql, $lastWeekDate);
+        FROM t_gold2receive WHERE create_time <= ? AND receive_status = 1';
+$return = $db->exec($sql, $lastDayTime);
 
  if ($return) {
-    $sql = 'DELETE FROM t_gold2receive WHERE receive_date <= ?';
-    $db->exec($sql, $lastWeekDate); 
+    $sql = 'DELETE FROM t_gold2receive WHERE create_time <= ?';
+    $db->exec($sql, $lastDayTime); 
  } else {
      echo '转移数据失败';
  }
