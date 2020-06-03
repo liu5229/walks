c9eb5ed278921a5a467d6a48aa7452861f1da638
faaat <zjf580@163.com>
2020-06-03 21:56:26 +0800

    scratch

--- controller/Activity2Controller.php
+++ controller/Activity2Controller.php
@@ -376,6 +376,7 @@ Class Activity2Controller extends AbstractController {
     public function scratchListAction () {
         $config = array(7, 11, 15, 20, 23);
         $nowHours = date('H');
+        $nowHours = 6;
         $todayDate = date('Y-m-d');
         $endTime = '';
         $batch = 0;
