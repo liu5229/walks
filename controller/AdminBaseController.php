3726a278ed5d09333124fc5c2c0edd357c9d97b0
faaat <zjf580@163.com>
2020-06-09 11:56:04 +0800

    change menu

--- controller/AdminBaseController.php
+++ controller/AdminBaseController.php
@@ -60,26 +60,34 @@ Class AdminBaseController extends AbstractController {
 //      },
 //    ],
         return  array('list' => array(
-            array( 'id' => 1, 'resName' => '首页', 'resKey'=> 'index', 'resIcon'=> 'statistics'),
-//            array( 'id' => 3, 'resName' => '活动管理', 'resKey'=> 'activity', 'resIcon'=> 'statistics'),
-            array( 'id' => 4, 'resName' => '运营位管理', 'resKey'=> 'ad', 'resIcon'=> 'moduleManage'),
-            array( 'id' => 2, 'resName' => '用户管理', 'resKey'=> 'menu_user', 'resIcon'=> 'xtxg', 'children' => array(
-                array( 'id' => 201, 'resName' => '用户明细', 'resKey'=> 'user', 'resIcon'=> 'userManage'),
-                array( 'id' => 202, 'resName' => '用户提现', 'resKey'=> 'withdraw', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 203, 'resName' => '用户反馈', 'resKey'=> 'feedback', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 204, 'resName' => '用户邀请', 'resKey'=> 'invited', 'resIcon'=> 'moduleManage'),
+            array('id' => '1', 'resName' => '计步宝', 'resKey'=> 'menu_stepcounter', 'resIcon'=> 'xtxg', 'children' => array(
+                array( 'resName' => '首页', 'resKey'=> 'index'),
+                array( 'resName' => '运营位管理', 'resKey'=> 'ad'),
+                array( 'id' => '1-1', 'resName' => '用户管理', 'resKey'=> 'menu_stepcounter_user', 'children' => array(
+                    array( 'resName' => '用户明细', 'resKey'=> 'user'),
+                    array( 'resName' => '用户提现', 'resKey'=> 'withdraw'),
+                    array( 'resName' => '用户反馈', 'resKey'=> 'feedback'),
+                    array( 'resName' => '用户邀请', 'resKey'=> 'invited'),
+                )),
+                array( 'id' => '1-2', 'resName' => '系统管理', 'resKey'=> 'menu_stepcounter_system', 'children' => array(
+                    array( 'resName' => '版本升级', 'resKey'=> 'version'),
+                    array( 'resName' => '广告频闭', 'resKey'=> 'version-ad'),
+                    array( 'resName' => '三方错误码', 'resKey'=> 'sdk-error'),
+                )),
             )),
-            array( 'id' => 6, 'resName' => '系统管理', 'resKey'=> 'menu_system', 'resIcon'=> 'moduleManage', 'children' => array(
-                array( 'id' => 601, 'resName' => '版本升级', 'resKey'=> 'version', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 602, 'resName' => '广告频闭', 'resKey'=> 'version-ad', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 603, 'resName' => '三方错误码', 'resKey'=> 'sdk-error', 'resIcon'=> 'moduleManage'),
+            array('id' => '2', 'resName' => '狗狗世界', 'resKey'=> 'menu_dogsworld', 'resIcon'=> 'moduleManage', 'children' => array(
+                array( 'id' => '2-1', 'resName' => '用户管理', 'resKey'=> 'menu_dogsworld_user', 'children' => array(
+                    array( 'resName' => '内部用户', 'resKey'=> 'dogs-interior'),
+                    array( 'resName' => '用户提现', 'resKey'=> 'dogs-withdraw'),
+                    array( 'resName' => '用户列表', 'resKey'=> 'dogs-user'),
+                )),
+                array( 'id' => '2-2', 'resName' => '系统管理', 'resKey'=> 'menu_dogsworld_system', 'children' => array(
+                    array( 'resName' => '版本升级', 'resKey'=> 'dogs-version'),
+                    array( 'resName' => '广告频闭', 'resKey'=> 'dogs-version-ad'),
+                )),
             )),
-            array( 'id' => 7, 'resName' => '狗狗世界', 'resKey'=> 'dogsworld', 'resIcon'=> 'moduleManage', 'children' => array(
-                array( 'id' => 601, 'resName' => '版本升级', 'resKey'=> 'dogs-version', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 602, 'resName' => '广告频闭', 'resKey'=> 'dogs-version-ad', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 603, 'resName' => '内部用户', 'resKey'=> 'dogs-interior', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 604, 'resName' => '用户提现', 'resKey'=> 'dogs-withdraw', 'resIcon'=> 'moduleManage'),
-                array( 'id' => 604, 'resName' => '用户列表', 'resKey'=> 'dogs-list', 'resIcon'=> 'moduleManage'),
+            array('id' => '3', 'resName' => '广告配置', 'resKey'=> 'menu_ad', 'resIcon'=> 'xtxg', 'children' => array(
+                array( 'resName' => '开发中...', 'resKey'=> 'ad-user'),
             )),
         ));
     }
