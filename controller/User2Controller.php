b479bf55c9755ac9f512c9205e976b2daadfd73a
faaat <zjf580@163.com>
2020-06-29 22:36:58 +0800

    add start left ad

--- controller/User2Controller.php
+++ controller/User2Controller.php
@@ -130,7 +130,7 @@ Class User2Controller extends UserController {
             return $userId;
         }
         //start 首页底部  top 任务页 头部  new 任务页新手任务  daily 任务页日常任务  my 我的页面 右上角  dogs 我的页面狗狗世界导流
-        $adCount = array('start' => 3, 'top' => 4, 'new' => 0, 'daily' => 0, 'my' => 1, 'dogs' => 0, 'start_2' => 3);
+        $adCount = array('start' => 3, 'top' => 4, 'new' => 0, 'daily' => 0, 'my' => 1, 'dogs' => 0, 'start_2' => 3, 'start_left' => 1);
         if (!isset($this->inputData['location']) || !in_array($this->inputData['location'], array_keys($adCount))) {
             return new ApiReturn('', 205, '访问失败，请稍后再试');
         }
