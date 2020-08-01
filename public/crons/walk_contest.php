09a6179568d18d82030b08a58460e219cbce1376
faaat <zjf580@163.com>
2020-08-01 14:50:43 +0800

    fix walk contest script

--- public/crons/walk_contest.php
+++ public/crons/walk_contest.php
@@ -79,7 +79,7 @@ function updateData($date, $count) {
         }
         // 添加 虚拟用户
         $addUser = $virtualConfig[$contestInfo['contest_level']][$count];
-        var_dump($virtualConfig[$contestInfo['contest_level']]);
+        var_dump($virtualConfig);
         // 添加 虚拟完成用户
         $virtualUser = $contestInfo['virtual_count'] + rand($addUser['min'], $addUser['max']);
         $virtualComplete = 0;
