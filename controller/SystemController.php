2992ae7672bdf5b3f7a35539bc5e7f8159794322
faaat <zjf580@163.com>
2020-08-02 14:36:31 +0800

    interactionExpress config

--- controller/SystemController.php
+++ controller/SystemController.php
@@ -18,6 +18,6 @@ Class SystemController extends AbstractController {
 
     public function ieConfigAction () {
 
-        return array('newer1' => array(2, 7, 12, 17, 22), 'newer2' => array(2, 12, 22, 32, 42), 'normal' => array(15, 60));
+        return new ApiReturn(array('newer1' => array(2, 7, 12, 17, 22), 'newer2' => array(2, 12, 22, 32, 42), 'normal' => array(15, 60)));
     }
 }
