28cb2dfb1de25862821a42c6adbec419b7a7cec1
faaat <zjf580@163.com>
2020-08-05 13:35:20 +0800

    fix walk contest sql

--- public/crons/walk_contest.php
+++ public/crons/walk_contest.php
@@ -30,11 +30,11 @@ if (!$execCount) {
         // 参与的用户+参与的虚拟用户 total
         // 完成的用户+完成的虚拟用户 complete
         // 计算每个完成的用户奖励 total * 档位的奖励 / complete 向上取整
-        $award = ceil($contestInfo['total_count'] * $awardConfig[$contestInfo['contest_level']] / $contestInfo['complete_count']);
+        $award = $contestInfo['complete_count'] ? ceil($contestInfo['total_count'] * $awardConfig[$contestInfo['contest_level']] / $contestInfo['complete_count']) : 0;
         //发放奖励
         $sql = "INSERT INTO t_gold2receive (user_id, receive_gold, receive_walk, receive_type, receive_date) VALUES";
         foreach ($realCompleteUser as $userId) {
-            $sql .= '(' . $userId . ', ' . $award . ', ' . $contestInfo['contest_level'] . ', "walk_contest"' . '"' . date('Y-m-d') . '"),';
+            $sql .= '(' . $userId . ', ' . $award . ', ' . $contestInfo['contest_level'] . ', "walk_contest", ' . '"' . date('Y-m-d') . '"),';
         }
         $sql = rtrim($sql,',');
         $db->exec($sql);
